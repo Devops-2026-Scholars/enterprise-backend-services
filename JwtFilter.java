d99ad66142d4b633482a06f2d443ead03279bb3e
Joy Onitsha <onitshajoy50@gmail.com>
2026-08-28 22:56:22 +0100

    Add Joy's practice line to JwtFilter

--- JwtFilter.java
+++ JwtFilter.java
@@ -5,5 +5,7 @@ public class JwtFilter {
    public static void main(String[] args) {
 	System.out.println("This is a JWT Filter in this app.");
 	System.out.println("This was added by me, Rhume");
+System.out.println("Joy was here - practicing Git!");
+
    }
 }
